42a0d8ce672f75b98c40ef49aa7432de32ac3fca
sirijiji <sirisak_01@yahoo.fr>
2023-09-27 11:33:52 +0200

    first commit

---
+++ src/main/java/com/example/exampleSpringboot320M3/ExampleSpringboot320M3Application.java
@@ -0,0 +1,13 @@
+package com.example.exampleSpringboot320M3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringboot320M3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringboot320M3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exampleSpringboot320M3/ExampleSpringboot320M3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleSpringboot320M3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringboot320M3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
