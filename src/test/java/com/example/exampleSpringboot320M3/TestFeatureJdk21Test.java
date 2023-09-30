3e7b9cfc38cdf0837669d0da92e98bba239b08d0
sirijiji <sirisak_01@yahoo.fr>
2023-09-30 20:36:09 +0200

    fix config for lombok

--- src/test/java/com/example/exampleSpringboot320M3/TestFeatureJdk21Test.java
+++ src/test/java/com/example/exampleSpringboot320M3/TestFeatureJdk21Test.java
@@ -1,5 +1,6 @@
 package com.example.exampleSpringboot320M3;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -15,6 +16,7 @@ public class TestFeatureJdk21Test {
             IntStream.range(0, 10_000).forEach(i -> {
                 executor.submit(() -> {
                     Thread.sleep(Duration.ofSeconds(1));
+                    System.out.println(String.valueOf(i));
                     return i;
                 });
             });
