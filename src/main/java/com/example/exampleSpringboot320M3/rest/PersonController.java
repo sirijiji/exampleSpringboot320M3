e8056fe06fb6063f3cb8ad8b2592af4d89b83146
sirijiji <sirisak_01@yahoo.fr>
2023-11-23 21:26:59 +0100

    upgrade to spring boot 21

---
+++ src/main/java/com/example/exampleSpringboot320M3/rest/PersonController.java
@@ -0,0 +1,16 @@
+package com.example.exampleSpringboot320M3.rest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PersonController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
