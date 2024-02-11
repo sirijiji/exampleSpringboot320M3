aed9e52ba5f7a7b3804717b29dad23faf0aa89eb
sirijiji <sirisak_01@yahoo.fr>
2024-02-11 13:33:21 +0100

    add example of completable future with virtual thread

--- src/test/java/com/example/exampleSpringboot320M3/TestFeatureJdk21Test.java
+++ src/test/java/com/example/exampleSpringboot320M3/TestFeatureJdk21Test.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 @Slf4j
@@ -16,15 +18,40 @@ public class TestFeatureJdk21Test {
 
     @Test
     public void checkTEst(){
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            IntStream.range(0, 10_000).forEach(i -> {
-                executor.submit(() -> {
-                    Thread.sleep(Duration.ofSeconds(1));
-                    log.info(String.valueOf(i));
-                    return i;
-                });
+//        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+//            IntStream.range(0, 10_000).forEach(i -> {
+//                executor.submit(() -> {
+//                    Thread.sleep(Duration.ofSeconds(1));
+//                    log.info(String.valueOf(i));
+//                    return i;
+//                });
+//            });
+//        }  // executor.close() is called implicitly, and waits
+
+
+        try(ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+
+            CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
+                log.info("Running a task in a virtual thread 1");
+            }, executor);
+
+
+            CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> {
+                log.info("Running a task in a virtual thread 2");
+            }, executor);
+
+
+            CompletableFuture.allOf(future1, future2).whenComplete((v, t) -> {
+              log.info("complete future1 and future2");
             });
-        }  // executor.close() is called implicitly, and waits
+
+            log.info(Thread.currentThread().getName());
+        }
+
+
+
+
+
     }
 
 
