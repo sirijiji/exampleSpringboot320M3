fc8bab7e9e054574a79c68fea22387937aca05ed
sirijiji <sirisak_01@yahoo.fr>
2025-03-20 21:41:02 +0100

    upgrade spring boot to 3.4.3

--- src/main/java/com/example/exampleSpringboot320M3/rest/PersonController.java
+++ src/main/java/com/example/exampleSpringboot320M3/rest/PersonController.java
@@ -1,6 +1,10 @@
 package com.example.exampleSpringboot320M3.rest;
+
+import com.example.exampleSpringboot320M3.record.Person;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,6 +15,14 @@ public class PersonController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/greeting/test", produces = "application/json")
+    public ResponseEntity<Person> greetingTest() {
+
+        Person person = new Person("nom", "prenom");
+        return ResponseEntity.ok(person);
     }
 
 }
--- src/main/java/com/example/exampleSpringboot320M3/scheduler/SchedulerActions.java
+++ src/main/java/com/example/exampleSpringboot320M3/scheduler/SchedulerActions.java
@@ -3,7 +3,6 @@ package com.example.exampleSpringboot320M3.scheduler;
 import com.example.exampleSpringboot320M3.ExampleEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -12,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class SchedulerActions {
 
 
-    @Scheduled(fixedDelay = 10000)
+//    @Scheduled(fixedDelay = 10000)
     public void scheduled() {
         log.info("scheduled");
         log.info(String.format(ExampleEnum.TEST1.getLabel(), "1", "2"));
